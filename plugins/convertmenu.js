e6b7ffcc37567f199f5c5fac43be4333126fad37
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-11 23:54:00 +0500

    Update convertmenu.js

--- plugins/convertmenu.js
+++ plugins/convertmenu.js
@@ -41,7 +41,17 @@ await conn.sendMessage(
 from,
 {
 image:{ url: config.MENU_IMAGE_URL || "https://i.ibb.co/TxSCwf8B/temp.jpg" },
-caption: dec
+caption: dec,
+contextInfo:{
+mentionedJid:[m.sender],
+forwardingScore:999,
+isForwarded:true,
+forwardedNewsletterMessageInfo:{
+newsletterJid:"120363424512151830@newsletter",
+newsletterName:"Zᴀʜɪᴅ Kɪɴɢ",
+serverMessageId:143
+}
+}
 },
 { quoted: mek }
 )
