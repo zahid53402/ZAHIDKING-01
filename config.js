ef75c8815c2d552a3146f29f37082f58a1a97c08
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-11 22:36:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
-GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
+GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyBLYOF8elDrc2_DeGJr118NWuXCyV0VB8Y",
 
 CHATBOT: process.env.CHATBOT || "",
 
