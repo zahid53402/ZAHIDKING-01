721d6b01d68d423558bc8e1c986c7d885a0b968a
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-12 21:25:26 +0500

    Update gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -1,9 +1,8 @@
 const axios = require("axios")
 
 module.exports = {
-pattern: "gemini",
-alias: ["ai"],
-desc: "Chat with AI",
+pattern: "ai",
+desc: "AI chat",
 category: "ai",
 react: "🤖",
 
@@ -11,23 +10,22 @@ async function(conn, m, { q, reply }) {
 
 try {
 
-if (!q) return reply("❌ Example:\n.gemini Hello")
+if (!q) return reply("Example:\n.ai Hello")
 
-let res = await axios.get(`https://api.simsimi.net/v2/?text=${encodeURIComponent(q)}&lc=en`)
-
-let data = res.data
-
-if (!data || !data.success) {
-return reply("❌ AI no response")
+let res = await axios.get("https://api.affiliateplus.xyz/api/chatbot", {
+params: {
+message: q,
+botname: "ZahidAI",
+ownername: "Zahid",
+user: m.sender
 }
+})
 
-reply(data.success)
+reply(res.data.message)
 
 } catch (e) {
-
 console.log(e)
 reply("❌ AI Error")
-
 }
 
 }
