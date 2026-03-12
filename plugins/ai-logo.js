b680a363fb22aa474db78d07fc9a561e1c0d261e
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-12 06:59:21 +0500

    Update ai-logo.js

--- plugins/ai-logo.js
+++ plugins/ai-logo.js
@@ -1,4 +1,5 @@
 const { cmd } = require('../command')
+const axios = require('axios')
 
 cmd({
 pattern: "ailogo",
@@ -16,10 +17,21 @@ if(!args[0]) return reply("Example: .ailogo Zahid King")
 
 const text = args.join(" ")
 
-const url = "https://image.pollinations.ai/prompt/" + encodeURIComponent(`anime boy poster with big golden text ${text}`)
+const prompt = `anime boy poster with big golden text ${text} ultra hd`
+
+const url = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`
+
+// image download
+const response = await axios({
+url: url,
+method: "GET",
+responseType: "arraybuffer"
+})
+
+const buffer = Buffer.from(response.data)
 
 await conn.sendMessage(from,{
-image:{ url:url },
+image: buffer,
 caption:`✨ AI Logo Generated
 
 Name : ${text}
