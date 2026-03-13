c789a7f1ac8cef59dd96ff29b0b2b8d5988128e9
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-13 15:42:29 +0500

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -10,42 +10,35 @@ filename: __filename
 },
 async (conn, m, msg, { args, reply, pushname }) => {
 
-try {
+try{
 
 if(!args[0]) return reply("Example:\n.ai hello");
 
-let question = args.join(" ");
-
-let system = `
-You are Zahid King AI Assistant created by Zahid King.
-Always respond friendly.
-
-User name: ${pushname}
-
-Always add this line at the end:
-Powered by Zahid King
-`;
+let q = args.join(" ")
 
 const res = await axios.post(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${config.GEMINI_API_KEY}`,
 {
-contents: [
-{
-parts: [{ text: system + "\nUser: " + question }]
-}
-]
-}
-);
+contents:[{
+parts:[{
+text:`You are Zahid King AI Assistant created by Zahid King.
+User name: ${pushname}
 
-let text = res.data.candidates[0].content.parts[0].text;
+User: ${q}
 
-reply(text);
+Always end reply with:
+Powered by Zahid King`
+}]
+}]
+})
 
-} catch(e) {
+let text = res.data.candidates[0].content.parts[0].text
 
-console.log(e);
-reply("❌ AI error");
+reply(text)
 
+}catch(e){
+console.log(e)
+reply("❌ AI error")
 }
 
 });
